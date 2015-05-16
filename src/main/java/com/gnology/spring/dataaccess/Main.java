e8128e5ec10cbff5009fe22f3f2b4af7b3b84568
Damian Ujma <midosinid@yandex.ru>
2015-05-16 15:14:17 +0200

    new
    new stuff

--- src/main/java/com/gnology/spring/dataaccess/Main.java
+++ src/main/java/com/gnology/spring/dataaccess/Main.java
@@ -1,13 +1,14 @@
 package com.gnology.spring.dataaccess;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfiguration.class);
+        ApplicationContext context = new ClassPathXmlApplicationContext("AppConfig.xml");
         ProductsRepository productsRepository = context.getBean("productsRepository", ProductsRepository.class);
-        System.out.println( productsRepository.getNumberOfProducts());
+        int numberOfProducts = productsRepository.getNumberOfProducts();
+        System.out.println("Number of products: " + numberOfProducts);
 
     }
 }
--- src/main/java/com/gnology/spring/dataaccess/ProductsRepository.java
+++ src/main/java/com/gnology/spring/dataaccess/ProductsRepository.java
@@ -1,11 +1,21 @@
 package com.gnology.spring.dataaccess;
 
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
 
 @Repository
 public class ProductsRepository {
 
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public void setDataSource(MysqlDataSource dataSource) {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
     public int getNumberOfProducts() {
-        return 0;
+        return jdbcTemplate.queryForObject("select count(*) from products", Integer.class);
     }
 }
